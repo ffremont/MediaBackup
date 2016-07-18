7ce935f5d2d85332e0daa6459412a6198b5bbfd8
Florent FREMONT <ff.fremont.florent@gmail.com>
2016-07-18 17:24:25 +0200

    init

---
+++ src/main/java/com/github/ffremont/mediabackup/MediaBackupApp.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.ffremont.mediabackup;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.text.DateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+/**
+ *
+ * @author florent
+ */
+public class MediaBackupApp {
+
+    private static final int DATE_TIME_ORIGINAL = 36867;
+    private static Map<Path, LocalDateTime> dates = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        Path from = Paths.get("from");
+        Path to = Paths.get("to");
+
+        Files.walk(from).forEach(filePath -> {
+            if (Files.isRegularFile(filePath)) {
+                System.out.println(filePath);
+                try {
+                    Metadata metadata = ImageMetadataReader.readMetadata(filePath.toAbsolutePath().toFile());
+                    Iterable<Directory> it = metadata.getDirectories();
+                    it.forEach((Directory dir) -> {
+                        dir.getTags().forEach((Tag tag) -> {
+                            if (tag.getTagType() == 36867) {
+                                System.out.println("--------------");
+                                System.out.println(tag.getTagName());
+                                System.out.println(tag.getTagType());
+                                System.out.println(tag.getTagTypeHex());
+                                System.out.println(dir.getDate(tag.getTagType()));
+                                System.out.println("--------------");
+                                dates.put(filePath, LocalDateTime.ofInstant(dir.getDate(tag.getTagType()).toInstant(), ZoneId.systemDefault()));
+                            }
+                        });
+                    });
+                } catch (IOException ex) {
+                    Logger.getLogger(MediaBackupApp.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (ImageProcessingException ex) {
+                    Logger.getLogger(MediaBackupApp.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+
+        // chaque fichiers
+        dates.entrySet().stream().forEach((Map.Entry<Path, LocalDateTime> entry) -> {
+            try {
+                Path destination = Paths.get(to.toAbsolutePath().toString(),
+                        "" + entry.getValue().getYear(),
+                        "" + entry.getValue().format(DateTimeFormatter.ofPattern("MMMM", Locale.FRENCH)),
+                        entry.getValue().getDayOfMonth() + "");
+                Files.createDirectories(destination);
+                //Metadata metadata = ImageMetadataReader.readMetadata(imagePath);
+                Files.copy(
+                        entry.getKey(), 
+                        Paths.get(
+                                destination.toAbsolutePath().toString(), 
+                                entry.getKey().getFileName().toString()
+                        ), StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+
+    }
+}
