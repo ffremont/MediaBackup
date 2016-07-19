f15c4b1c9274cbcb750f4cc7804572da09c8a5da
Florent FREMONT <ff.fremont.florent@gmail.com>
2016-07-19 16:54:28 +0200

    optimisation executor service

--- src/main/java/com/github/ffremont/mediabackup/MediaBackupApp.java
+++ src/main/java/com/github/ffremont/mediabackup/MediaBackupApp.java
@@ -10,24 +10,23 @@ import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.text.DateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Stream;
 
 /**
  *
@@ -36,15 +35,29 @@ import java.util.stream.Stream;
 public class MediaBackupApp {
 
     private static final int DATE_TIME_ORIGINAL = 36867;
-    private static Map<Path, LocalDateTime> dates = new HashMap<>();
-    private static Long blockCount;
+    private static Map<LocalDateTime, MetaFile> dates = new TreeMap<>();
 
-    public static void main(String[] args) throws IOException {
-        long blockSize = args.length > 0 ? Long.valueOf(args[0]) : -1;
+    private static ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2);
 
-        Path from = Paths.get("from");
-        Path to = Paths.get("to");
-        Path blocs = Paths.get("blocs");
+    private static void clear(Path directoryToDelete) {
+        try {
+            Files.walk(directoryToDelete).
+                    sorted((a, b) -> b.compareTo(a)). // reverse; files before dirs
+                    forEach(p -> {
+                        try {
+                            Files.delete(p);
+                        } catch (IOException e) { /* ... */ }
+                    });
+        } catch (IOException ex) {
+            Logger.getLogger(MediaBackupApp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        long blocsize = System.getProperty("blocsize") != null ? Long.valueOf(System.getProperty("blocsize")) : -1;
+        Path from = System.getProperty("from") != null ? Paths.get(System.getProperty("from")) : Paths.get("from");
+        Path to = System.getProperty("to") != null ? Paths.get(System.getProperty("to")) : Paths.get("to");
+        clear(to);
 
         Files.walk(from).forEach(filePath -> {
             if (Files.isRegularFile(filePath)) {
@@ -55,14 +68,13 @@ public class MediaBackupApp {
                     it.forEach((Directory dir) -> {
                         dir.getTags().forEach((Tag tag) -> {
                             if (tag.getTagType() == DATE_TIME_ORIGINAL) {
-                                /*System.out.println("--------------");
-                                 System.out.println(tag.getTagName());
-                                 System.out.println(tag.getTagType());
-                                 System.out.println(tag.getTagTypeHex());
-                                 System.out.println(dir.getDate(tag.getTagType()));
-                                 System.out.println("--------------");*/
                                 System.out.println(filePath);
-                                dates.put(filePath, LocalDateTime.ofInstant(dir.getDate(tag.getTagType()).toInstant(), ZoneId.systemDefault()));
+                                try {
+                                    LocalDateTime created = LocalDateTime.ofInstant(dir.getDate(tag.getTagType()).toInstant(), ZoneId.systemDefault());
+                                    dates.put(created, new MetaFile(created, filePath, Files.size(filePath)));
+                                } catch (IOException ex) {
+                                    Logger.getLogger(MediaBackupApp.class.getName()).log(Level.SEVERE, null, ex);
+                                }
                             }
                         });
                     });
@@ -74,39 +86,26 @@ public class MediaBackupApp {
             }
         });
 
-        // chaque fichiers
-        dates.entrySet().stream().forEach((Map.Entry<Path, LocalDateTime> entry) -> {
-            try {
-                Path destination = Paths.get(to.toAbsolutePath().toString(),
-                        "" + entry.getValue().getYear(),
-                        "" + entry.getValue().format(DateTimeFormatter.ofPattern("MMMM", Locale.FRENCH)),
-                        entry.getValue().getDayOfMonth() + "");
-                Files.createDirectories(destination);
-                //Metadata metadata = ImageMetadataReader.readMetadata(imagePath);
-                Files.copy(
-                        entry.getKey(),
-                        Paths.get(
-                                destination.toAbsolutePath().toString(),
-                                entry.getKey().getFileName().toString()
-                        ), StandardCopyOption.REPLACE_EXISTING);
-            } catch (IOException ex) {
-                ex.printStackTrace();
+        int i = 0;
+        long sum = 0;
+        List<Future> calls = new ArrayList<>();
+        for (Map.Entry<LocalDateTime, MetaFile> entry : dates.entrySet()) {
+            if ((blocsize != -1) && (sum + entry.getValue().getSize() > blocsize)) {
+                i++;
             }
-        });
 
-        if (blockSize != -1) {
-            int blocNb = 1;
-            blockCount = new Long(0);
-            Files.walk(from).forEach((Path filePath) -> {
-                if (Files.isRegularFile(filePath)) {
-                    try {
-                        blockCount += Files.size(filePath);
-                    } catch (IOException ex) {
-                        Logger.getLogger(MediaBackupApp.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    
-                }
-            });
+            calls.add(executor.submit(new CopyFile(to, i, entry.getValue())));
+            sum += entry.getValue().getSize();
         }
+
+        for(Future f : calls){
+            try {
+                f.get();
+            } catch (ExecutionException ex) {
+                Logger.getLogger(MediaBackupApp.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        executor.shutdown();
     }
+
 }
